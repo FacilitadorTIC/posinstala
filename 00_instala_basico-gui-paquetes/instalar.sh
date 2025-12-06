da4043ac35ad2f335966a4011045e04ab1ad653b
FacilitadorTIC <69331016+FacilitadorTIC@users.noreply.github.com>
2025-12-06 11:08:03 -0300

    --

--- 00_instala_basico-gui-paquetes/instalar.sh
+++ 00_instala_basico-gui-paquetes/instalar.sh
@@ -7,7 +7,7 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Instala...
-echo -e "\e[1mInstalling packages...\e[0m"
+echo -e "\e[1mInstalando packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
 apt-get install -y vlc gmtp synaptic galternatives evince
 apt-get install -y firmware-linux-nonfree
